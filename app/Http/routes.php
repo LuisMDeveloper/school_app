9a7660e66436e767f912d342e4bacd5e1a99640d
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-03-21 22:55:51 -0600

    Add index view for alumnos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Alumno;
+
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -15,16 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+Route::get('/alumnos', function() {
+
+    $alumnos = Alumno::all();
+
+    return view('alumnos.index')->with('alumnos', $alumnos);
+});
 
 Route::group(['middleware' => ['web']], function () {
     //
---
+++ resources/views/alumnos/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">Alumnos</div>
+        <div class="panel-body">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Nombre</td>
+                    <td>Apellidos</td>
+                    <td>Fecha de nacimiento</td>
+                    <td>Teléfono</td>
+                    <td>Nombre del tutor</td>
+                    <td>Teléfono de emergencia</td>
+                    <td>Facebook</td>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($alumnos as $key => $alumno)
+                        <tr>
+                            <td>{{ $alumno->id }}</td>
+                            <td>{{ $alumno->persona->nombre }}</td>
+                            <td>{{ $alumno->persona->apellidos }}</td>
+                            <td>{{ $alumno->persona->fecha_nacimiento }}</td>
+                            <td>{{ $alumno->persona->telefono }}</td>
+                            <td>{{ $alumno->nombre_del_tutor }}</td>
+                            <td>{{ $alumno->num_emergencia }}</td>
+                            <td>{{ $alumno->facebook }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <ul>
+
+            </ul>
+        </div>
+    </div>
+@endsection
